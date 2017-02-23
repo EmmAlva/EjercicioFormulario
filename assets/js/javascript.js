b2b871aa1860d125fc277ca0aa1cba2efdc237ca
Emma Tapia <clioemma_28@hotmail.com>
2017-02-23 12:51:10 -0500

    Projecto4-form2

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,9 +1,23 @@
 function fichaCompleta(){
+
 	var elemento = document.getElementById("resultado");
-	elemento.innerHTML = 
-	"<ul><li>Nombre: "+document.getElementById("nombre").value + "</li>"+
+
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
+	var dni = document.getElementById("dni").value
+	var direccion = document.getElementById("direccion").value
+	
+	if(nombre!== "" && apellido !== "" && dni !== "" && direccion!== ""){
+		elemento.innerHTML = 
+		"<li>Nombre: "+ nombre + "</li>"+
 		"<li> Apellido : " + document.getElementById("apellido").value + "</li>"+
 		"<li>DNI :" + document.getElementById("dni").value + "</li>"+
 		"<li> Dirección: " + document.getElementById("direccion").value +"</li>";
-	"</ul>"
+
+	}
+
+	
+
+
+	
 		}
